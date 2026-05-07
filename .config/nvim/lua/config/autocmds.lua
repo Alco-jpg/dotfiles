c282da6f9634306e937f562b6c3bdd62f930132e
LaWankah <jordy@reuzenpanda.nl>
2026-05-07 15:54:46 +0200

    fixed go to commands

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -15,13 +15,13 @@ api.nvim_create_autocmd("LspAttach", {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-        local map = function(keys, fn, desc)
-            vim.keymap.set('n', keys, fn, { buffer = ev.buf, desc = desc })
+        local map = function(keys, fn, desc, opts)
+            vim.keymap.set('n', keys, fn, vim.tbl_extend('force', { buffer = ev.buf, desc = desc }, opts or {}))
         end
 
         map('gd',         vim.lsp.buf.definition,      'Go to definition')
         map('gD',         vim.lsp.buf.declaration,     'Go to declaration')
-        map('gr',         vim.lsp.buf.references,      'Go to references')
+        map('gr',         vim.lsp.buf.references,      'Go to references', { nowait = true })
         map('gi',         vim.lsp.buf.implementation,  'Go to implementation')
         map('K',          vim.lsp.buf.hover,           'Hover docs')
         map('<leader>rn', vim.lsp.buf.rename,          'Rename symbol')
